b3ca41eebbbc80361258e26f61eb5a4a7eced3d9
Christopher <christopher.jeanne1@hotmail.com>
2018-01-24 13:51:34 +0100

    mise à jour de ma partie

--- src/controllers.php
+++ src/controllers.php
@@ -18,63 +18,7 @@ $app->get('/', function () use ($app) {
         return $app->redirect('index.php/accueil');
     }
 })
-->bind('homepage')
-;
-$app->get('/test', function () use ($app) {
-    if (isset($_GET['rechercher'])) {
-         $isbn = isset($_GET['isbn']) ? $_GET['isbn'] : '';
-         $request = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' . $isbn;
-         $response = file_get_contents($request);
-         $results = json_decode($response);
-        if ($results->totalItems > 0) {
-            $book = $results->items[0];
-            $infos['isbn'] = $book->volumeInfo->industryIdentifiers[0]->identifier;
-            $infos['titre'] = $book->volumeInfo->title;
-            $infos['auteur'] = $book->volumeInfo->authors[0];
-            $infos['langue'] = $book->volumeInfo->language;
-            $infos['pages'] = $book->volumeInfo->pageCount;
-            $infos['description'] = $book->volumeInfo->description;
-            return $app['twig']->render('formulaire_isbn.html.twig', array(
-                'ISBN' => "Numéro ISBN : ". $infos['isbn'],
-                'titre' => "Titre : ". $infos['titre'],
-                'auteur' => "Auteur : ". $infos['auteur'],
-                'langue' => "Langue : ". $infos['langue'],
-                'pages' => "Pages : ". $infos['pages'],
-                'description' => "Description : ". $infos['description']
-            ));
-        }
-        else {
-        return $app['twig']->render('formulaire_isbn.html.twig', array(
-          'echec' => "Livre introuvable"
-        ));
-        }
-    }
-    return $app['twig']->render('formulaire_isbn.html.twig', array());
-});
-$app->get('/about', function () use ($app){
-    return $app['twig']->render('about.html.twig', array());
-});
-$app->match('/accueil', function () use ($app){
-    return $app['twig']->render('accueil.html.twig', array());
-});
-$app->get('/catalogue_a_z', function () use ($app){
-    return $app['twig']->render('catalogue_a_z.html.twig', array());
-});
-$app->get('/catalogue_genre', function () use ($app){
-    return $app['twig']->render('catalogue_genre.html.twig', array());
-});
-$app->get('/recherche', function () use ($app){
-    return $app['twig']->render('recherche.html.twig', array());
-});
-$app->get('/contact', function () use ($app){
-    return $app['twig']->render('contact.html.twig', array());
-});
-//#loggin
-$app->match('/login', function (Request $request) use ($app){
-    return $app['twig']->render('login.html.twig', array(
-        'erreur' => $_GET['erreur'] ?? null,
-    ));
-});
+->bind('homepage');
 
 $app->match('/test', function () use ($app) {
     if (isset($_POST['rechercher'])) {
@@ -146,7 +90,11 @@ $app->match('/test', function () use ($app) {
 });
 
 $app->get('/about', function () use ($app){
-    return 'ok';
+    return $app['twig']->render('about.html.twig', array());
+});
+
+$app->match('/accueil', function () use ($app){
+    return $app['twig']->render('accueil.html.twig', array());
 });
 
 $app->get('/catalogue_a_z', function () use ($app){
@@ -166,9 +114,8 @@ $app->get('/recherche', function () use ($app){
 });
 
 $app->get('/contact', function () use ($app){
-    //return $app['twig']->render('contact.html.twig', array());
+    return $app['twig']->render('contact.html.twig', array());
 });
-
 
 //#loggin
 $app->match('/login', function (Request $request) use ($app){
